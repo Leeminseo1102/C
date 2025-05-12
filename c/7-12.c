5337129284f09edd13726ebbafdac7bb6345f978
이 민 서 (Lee min seo) <lmsjtsljh@naver.com>
2025-05-12 09:58:30 +0900

    Add files via upload

---
+++ c/7-12.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void main()
+{
+	int hap = 0;
+	int i;
+
+	for( i=1; i<=100;i++){
+		hap += i;
+
+		printf("1부터 100까지의 합은 %d입니다 \n", hap);
+		return;
+
+		printf("프로그렘의 끝입니다.\n");
+	}
+}
+
